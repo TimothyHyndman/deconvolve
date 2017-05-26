90fdc02c75f12a3f8f4bb1088eefae544ad4b14a
Timothy Hyndman <timothy.hyndman@gmail.com>
2017-05-26 15:26:05 +1000

    Continue nloptr code
    Still not really working, but seems to be getting closer. Might just need some tweaking.

--- R/DeconvolveSymmetricErrorPmf.R
+++ R/DeconvolveSymmetricErrorPmf.R
@@ -19,14 +19,6 @@
 #' 
 #' @export
 
-# There are two ways we can solve this optimization problem. 
-# 1. Find T* = min(T(p)) then minimize variance under constraint that T(p) = T*
-#
-# 2. Minimize var + lambda*T(p) for some suitable lambda.
-#
-# Option 1 is preferable I think, but harder to implement since T(p) = T* is a 
-# very tight constraint
-
 DeconvolveSymmetricErrorPmf <- function(W, m = 10){
 
 	n <- length(W)
@@ -62,59 +54,23 @@ DeconvolveSymmetricErrorPmf <- function(W, m = 10){
 	#--------------------------------------------------------------------------#
 
 	# Initial point for optimization routine
-	theta0 <- seq( min(W), max(W), length.out = m )
+	theta0 <- seq( min(W) + 0.01, max(W) - 0.01, length.out = m )
 	p0 <- numeric( m - 1 ) + 1/m
 	x0 <- c(p0, theta0)
 
 	# Set up constraints in form Ax \geq b
-	A <- matrix(0, nrow = m, ncol = 2*m-1)
-	A[1:m-1, 1:m-1] <- diag( m - 1 )   #pj non-negative
-	b = numeric(m)
-	A[m, 1:m-1] = matrix(-1, nrow=1, ncol = m-1) #pj sum to less than 1
+	A <- matrix(0, nrow = m-1 + 1 + 2*m, ncol = 2*m-1)
+	A[1:m-1, 1:m-1] <- diag( m - 1 )   # pj non-negative
+	b = numeric(m + 2 * m)
+	A[m, 1:m-1] = matrix(-1, nrow=1, ncol = m-1) # pj sum to less than 1
 	b[m] = -1
-	
+	# thetaj greater than min(W)
+	A[(m + 1):(2 * m), m:(2 * m - 1)] <- diag(m)
+	b[(m+1):(2*m)] <- min(W)
+	# thetaj less than max(W)
+	A[(2 * m + 1):(3 * m), m:(2 * m - 1)] <- -diag(m)
+	b[(2*m+1):(3*m)] <- -max(W)
 
-	#----------------------------------#
-	# Option 1
-	#----------------------------------#
-
-	# Minimize T(p)
-
-	# min.tp.sol <- stats::constrOptim(x0, CalculateTp, NULL, A, b, phi.W = phi.W, 
-	# 						  weight = weight)
-	
-	# Minimize the variance subject to T(theta,p) not increasing
-	# This is currently not working well as optim can't find any feasible 
-	# points other than the initial.
-
-	# min.var.sol <- stats::constrOptim(min.tp.sol$par, CalculateVarwithConstr, NULL, A, 
-	# 						   b, max.tp = min.tp.sol$value, phi.W = phi.W, 
-	# 						   weight = weight)
-
-	# min.var.sol <- stats::constrOptim(min.tp.sol$par, CombinedObjective, NULL, 
-	# 								  A, b, phi.W = phi.W, weight = weight)
-
-	# x.sol <- min.var.sol$par
-	# p.sol <- c( x.sol[1:m-1], 1 - sum(x.sol[1:m-1]))
-	# theta.sol <- x.sol[m:(2 * m - 1)]
-
-	# return(list("support" = theta.sol, "probweights" = p.sol))
-
-
-	#------------------------------------#
-	# Option 2
-	#------------------------------------#
-
-	# min.comb.sol <- stats::constrOptim(x0, CombinedObjective, NULL, A, b, 
-	# 							phi.W = phi.W, weight = weight)
-
-	# x.sol <- min.comb.sol$par
-	# p.sol <- c( x.sol[1:m-1], 1 - sum(x.sol[1:m-1]))
-	# theta.sol <- x.sol[m:(2 * m - 1)]
-
-	#------------------------------------#
-	# Option 3
-	#------------------------------------#
 
 	min.tp.sol <- stats::constrOptim(x0, CalculateTp, NULL, A, b, phi.W = phi.W, 
 							  weight = weight)
@@ -125,20 +81,30 @@ DeconvolveSymmetricErrorPmf <- function(W, m = 10){
 	ub <- numeric(2*m-1) + 1
 	ub[m:(2 * m - 1)] <- max(W)
 
-	opts <- list("algorithm"="NLOPT_LD_SLSQP", "maxeval" = 1e4)
+	# opts <- list("algorithm"="NLOPT_LD_SLSQP", "maxeval" = 1e3, 
+	# 			 "xtol_rel" = 1e-4)
+	local.opts <- list( "algorithm" = "NLOPT_LD_MMA", 
+					  	"xtol_rel" = 1e-4 )
+	opts <- list( "algorithm"="NLOPT_LD_AUGLAG", 
+				  "maxeval" = 1e4, 
+				  "xtol_rel" = 1e-4, 
+				  "local_opts" = local.opts )
+	
 	min.var.sol <- nloptr::nloptr(min.tp.sol$par, eval_f = CalculateVar, 
 								  eval_grad_f = CalculateVarGrad, lb = lb, 
 								  ub = ub, eval_g_ineq = Constraints, 
-								  phi.W = phi.W, weight = weight,
-								  tp.max = min.tp.sol$value, opts = opts)
+								  eval_jac_g_ineq = ConstraintsGrad,
+								  opts = opts, phi.W = phi.W, weight = weight,
+								  tp.max = min.tp.sol$value)
 
-	#--------------------------------------------------------------------------#
-	# Return
-	#--------------------------------------------------------------------------#
+	x.sol <- min.var.sol$solution
+	p.sol <- c( x.sol[1:m-1], 1 - sum(x.sol[1:m-1]))
+	theta.sol <- x.sol[m:(2 * m - 1)]
+	plot(theta.sol, p.sol)
 
 	return(list("support" = theta.sol, "probweights" = p.sol, "phi.W" = phi.W))
 }
-
+#' @export
 CalculateTp <- function(x, phi.W, weight){
 	m <- (length(x) + 1) / 2
 
@@ -157,7 +123,7 @@ CalculateTp <- function(x, phi.W, weight){
 
 	return(tp)
 }
-
+#' @export
 CalculateVar <- function(x, phi.W, weight, tp.max){
 	m <- (length(x) + 1) / 2
 	p <- c( x[ 1:(m - 1) ], 1 - sum( x[ 1:(m - 1) ] ) )
@@ -166,54 +132,28 @@ CalculateVar <- function(x, phi.W, weight, tp.max){
 	var <- sum(p*(theta - mean)^2)
 	return(var)
 }
-
+#' @export
 CalculateVarGrad <- function(x, phi.W, weight, tp.max){
-	var.x <- CalculateVar(x, phi.W, weight, tp.max)
-	dx <- 0.01
-	grad <- numeric(length(x))
-
-	for (i in 1:length(x)){
-		y <- x
-		y[i] <- x[i] + dx
-		var.x1 <- CalculateVar(x1, phi.W, weight, tp.max)
-		grad[i] <- (var.x1 - var.x) / dx	
-	}
-	
-	return(grad)
+	numDeriv::grad(CalculateVar, x, method = "simple", phi.W = phi.W, weight = weight, 
+				   tp.max = tp.max)
 }
 
-# CalculateVarwithConstr <- function(x, max.tp, phi.W, weight){
-# 	#Check that tp hasn't gotten any bigger
-# 	tp <- CalculateTp(x, phi.W, weight)
-# 	if (tp > max.tp){
-# 		return(Inf)
-# 	}
-
-# 	# All constraints are met so calculate variance
-# 	var <- CalculateVar(x)
-# 	return(var)
-# }
-
-# CombinedObjective <- function(x, phi.W, weight, lambda = 10000){
-# 	CalculateVar(x) + lambda * CalculateTp(x, phi.W, weight)
-# }
-
+#' @export
 Constraints <- function(x, phi.W, weight, tp.max){
 	tp <- CalculateTp(x, phi.W, weight)
-	const1 <- tp - tp.max
+	const1 <- tp - tp.max	# Does giving it a little slack help?
 
 	m <- (length(x) + 1) / 2
 	p <- c( x[ 1:m - 1 ], 1 - sum( x[ 1: (m - 1) ] ) )
 	theta <- x[ m:(2 * m - 1) ]
 
-	const2 <- sum(p < 0)
-	const3 <- sum(p > 1)
+	const2 <- -sum(p[p < 0])
+	const3 <- sum(p[p > 1] - 1)
 
 	return(c(const1, const2, const3))
 }
-
+#' @export
 ConstraintsGrad <- function(x, phi.W, weight, tp.max){
-	tp.x <- CalculateTp(x, phi.W, weight, tp.max)
-	dx <- 0.01
-	grad <- numeric(length(x))
+	numDeriv::jacobian(Constraints, x, method = "simple", phi.W = phi.W, weight = weight,
+					   tp.max = tp.max)
 }
