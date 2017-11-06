533918fb1f8c026b509bbcc1a20efe9281ef0300
Unknown <timothy.hyndman@gmail.com>
2017-11-07 10:40:26 +1100

    Add kernel selection function to deconvolve

--- man/examples/reg_deconvolve_eg.R
+++ man/examples/reg_deconvolve_eg.R
@@ -1,6 +1,8 @@
-n <- 200
-sigX <- 1
-sigU <- 0.2
-data <- GenerateTestData(n, sigX, sigU, dist_type = "mix", error_type = "norm", create_Y = TRUE)
-xx <- seq(-2, 8, 0.1)
-output <- reg_deconvolve(xx = xx, W = data$W, Y = data$Y, errortype = "norm", sigU = 0.2)
+\dontrun{
+	n <- 200
+	sigX <- 1
+	sigU <- 0.2
+	data <- GenerateTestData(n, sigX, sigU, dist_type = "mix", error_type = "norm", create_Y = TRUE)
+	xx <- seq(-2, 8, 0.1)
+	output <- reg_deconvolve(xx = xx, W = data$W, Y = data$Y, errortype = "norm", sigU = 0.2)
+}
