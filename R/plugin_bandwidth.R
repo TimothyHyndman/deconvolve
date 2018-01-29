5e4d92bff3ed407913c8f9b7eda5edc76471a868
Unknown <timothy.hyndman@gmail.com>
2018-01-29 16:24:43 +1100

    Continue PI function

--- R/plugin_bandwidth.R
+++ R/plugin_bandwidth.R
@@ -1,17 +1,30 @@
-plugin_bandwidth <- function(W, phi_U, stdevx, phi_K, mu_K2, RK) {
-	  
+plugin_bandwidth <- function(W, phi_U, sd_X, kernel_type) {
+	 
+
+	kernel_list <- kernel(kernel_type)
+	phiK <- kernel_list$phik
+	mu_K2 <- kernel_list$muk2
+	RK <- kernel_list$rk
+	tt <- kernel_list$tt
+
+
 	# Grid of h on which to search for a solution
 	maxh <- (max(W) - min(W)) / 10
-	hnaive <- ((8 * sqrt(pi) * RK/3/mu_K2^2)^0.2) * sqrt(stats::var(W)) * n^(-1/5)
+	hnaive <- ((8 * sqrt(pi) * RK/3/mu_K2^2)^0.2) * sqrt(stats::var(W)) * 
+	n^(-1/5)
 	hgrid <- seq(hnaive / 3, maxh, length.out = 100)
 
+	# Quantities that will be needed multiple times below
+	toverh <- tt %*% t(1 / hgrid)
+	phiKsq <- phiK(tt)^2
+	phiUsq <- phiU(toverh)^2
 
 	# theta 4
-	th4 <- (st.dev.X^(-9) * factorial(8) ) / (2^9 * factorial(4) * sqrt(pi))
+	th4 <- (sd_X^(-9) * factorial(8) ) / (2^9 * factorial(4) * sqrt(pi))
 
 	# h3
 	rr <- 3
-	term1 <- -hgrid^2 * muK2 * th4
+	term1 <- -hgrid^2 * mu_K2 * th4
 	term2 <- kronecker(matrix(1, 1, lh), tt^(2*rr) * phiKsq) / phiUsq
 	term2 <- apply(term2, 2, sum) * deltat
 	term2 <- term2 / (2 * pi * n * hgrid^(2 * rr + 1))
