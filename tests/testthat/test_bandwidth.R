09d8f2cc6481ce380f6ac2c03b90feac3b8f1c56
Unknown <timothy.hyndman@gmail.com>
2018-05-10 15:39:38 +1000

    Fix tests so it passes check

--- tests/testthat/test_bandwidth.R
+++ tests/testthat/test_bandwidth.R
@@ -51,7 +51,7 @@ test_that("replicates SIMEX method 1", {
 	n <- 50
 	data <- GenerateTestData(n, sd_X, sd_U, dist_type = "chi", error_type = "norm", replicates = TRUE)
 	Y <- 2*data$W1
-	output_test <- bandwidth(data$W1, data$W2, algorithm = "SIMEX", Y = Y, seed = 100)
+	output_test <- bandwidth(data$W1, data$W2, algorithm = "SIMEX", Y = Y, seed = 100, n_cores = 2)
 	expect_equal(output_test$h, 0.07305357, tolerance = 0.0000001)
 	expect_equal(output_test$rho, 0.09636439, tolerance = 0.0000001)
 })
@@ -61,7 +61,7 @@ test_that("replicates SIMEX method 2", {
 	n <- 50
 	data <- GenerateTestData(n, sd_X, sd_U, dist_type = "chi", error_type = "norm", replicates = TRUE)
 	Y <- 2*data$W1
-	output_test <- bandwidth(data$W1, data$W2, algorithm = "SIMEX", Y = Y, seed = 100, use_alt_SIMEX_rep_opt = TRUE)
+	output_test <- bandwidth(data$W1, data$W2, algorithm = "SIMEX", Y = Y, seed = 100, use_alt_SIMEX_rep_opt = TRUE, n_cores = 2)
 	expect_equal(output_test$h, 0.06711634, tolerance = 0.0000001)
 	expect_equal(output_test$rho, 0.004956175, tolerance = 0.0000001)
 })
