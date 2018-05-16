b6dbdc854ab0c23a1591d32f57537e38bbd8f487
Unknown <timothy.hyndman@gmail.com>
2018-05-16 15:37:39 +1000

    Don't plot point masses with pdf
    Fixes a scaling issue and there's no real reason to show point masses if we're interested in a density

--- R/plot.deconvolve.R
+++ R/plot.deconvolve.R
@@ -13,26 +13,26 @@ plot.deconvolve <- function(x, ...){
 		plot_pdf <- ggplot2::geom_path(data = df_pdf, ggplot2::aes(xvec,yvec), 
 								   color = dens_col, size = 1)
 		final_plot <- final_plot + plot_pdf
-	}
+	} else {
+		if ("probweights" %in% names(x)) {
+			p <- x$probweights
+			theta <- x$support
+			df_pmf_sol <- data.frame(p, theta)
+			plot_pmf_sol <- ggplot2::geom_point(data = df_pmf_sol, 
+												ggplot2::aes(theta, p), 
+												color = dens_col)
+			final_plot <- final_plot + plot_pmf_sol
+		}
 
-	if ("probweights" %in% names(x)) {
-		p <- x$probweights
-		theta <- x$support
-		df_pmf_sol <- data.frame(p, theta)
-		plot_pmf_sol <- ggplot2::geom_point(data = df_pmf_sol, 
-											ggplot2::aes(theta, p), 
-											color = dens_col)
-		final_plot <- final_plot + plot_pmf_sol
-	}
-
-	if ("probweights_mintp" %in% names(x)) {
-		p_mintp <- x$probweights_mintp
-		theta_mintp <- x$support_mintp
-		df_mintp <- data.frame(p_mintp, theta_mintp)
-		plot_pmf_mintp <- ggplot2::geom_point(data = df_mintp, 
-			  							ggplot2::aes(theta_mintp, p_mintp), 
-			  							color = "magenta")
-		final_plot <- final_plot + plot_pmf_mintp
+		if ("probweights_mintp" %in% names(x)) {
+			p_mintp <- x$probweights_mintp
+			theta_mintp <- x$support_mintp
+			df_mintp <- data.frame(p_mintp, theta_mintp)
+			plot_pmf_mintp <- ggplot2::geom_point(data = df_mintp, 
+				  							ggplot2::aes(theta_mintp, p_mintp), 
+				  							color = "magenta")
+			final_plot <- final_plot + plot_pmf_mintp
+		}
 	}
 
 	# Add title ----------------------------------------------------------------
