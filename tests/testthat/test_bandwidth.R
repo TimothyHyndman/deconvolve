8bda87e8c27d9bfc115a53d324069029fa38b9a6
Unknown <timothy.hyndman@gmail.com>
2018-05-16 15:21:20 +1000

    Fix tests

--- tests/testthat/test_bandwidth.R
+++ tests/testthat/test_bandwidth.R
@@ -1,7 +1,7 @@
 library(deconvolve)
 context("Bandwidth")
 
-load("sym_error_test_result.RData")
+# load("sym_error_test_result.RData")
 
 set.seed(1)
 n <- 50
@@ -12,7 +12,7 @@ W <- GenerateTestData(n, sd_X, sd_U, dist_type = "chi", error_type = "norm")
 set.seed(1)
 test_that("homoscedastic errors case gives expected result", {
 	skip_on_cran()
-	expect_equal(bandwidth(W, errortype = "norm", sd_U = sd_U), 0.157268, tolerance = 0.0000001)
+	expect_equal(bandwidth(W, errortype = "norm", sd_U = sd_U), 0.1278101, tolerance = 0.0000001)
 })
 
 set.seed(1)
@@ -27,7 +27,7 @@ set.seed(1)
 data <- GenerateTestData(n, sd_X, sd_U, dist_type = "chi", error_type = "norm", replicates = TRUE)
 test_that("replicates case gives expected result", {
 	skip_on_cran()
-	expect_equal(bandwidth(data$W1, data$W2), 0.1253582, tolerance = 0.0000001)
+	expect_equal(bandwidth(data$W1, data$W2), 0.09959522, tolerance = 0.0000001)
 })
 
 set.seed(2)
@@ -35,7 +35,7 @@ n <- 500
 test_that("no error case gives expected result", {
 	skip_on_cran()
 	W <- GenerateTestData(n, sd_X, sd_U, dist_type = "chi", error_type = "norm")
-	expect_equal(bandwidth(W), 0.1171458, tolerance = 0.0000001)
+	expect_equal(bandwidth(W), 0.1104239, tolerance = 0.0000001)
 })
 
 set.seed(1)
